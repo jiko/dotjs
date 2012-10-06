fcdcfda697f9ac0c664f6fd1d884af2372b07746
Jim <jk@jameskoppen>
2012-10-06 00:20:10 -0500

    Replace the 'directory listing' header on bukk.it with a simple slideshow of said directory's contents.

--- bukk.it.js
+++ bukk.it.js
@@ -3,8 +3,22 @@
 var i = 4;
 var links = document.links;
 var stop = links.length;
+var interval = 10000;
 var heading = document.body.getElementsByTagName("h1")[0];
-function makeIMG(href) { var img = document.createElement("img"); img.src = href; return img; }
-function funLoop(imghndl) { setTimeout( function() { ++i; var hndl = makeIMG(links[i].href); console.log(hndl); document.body.replaceChild(hndl,imghndl); if ( i < stop ) { funLoop(hndl) }},10000); }
-function begin () { var first = makeIMG(links[i].href); document.body.replaceChild(first,heading); funLoop(first); return first; }
+function makeIMG(href) { 
+	var img = document.createElement("img");
+	img.src = href; return img; 
+}
+function funLoop(imghndl) { 
+	setTimeout( function() { 
+		++i; var hndl = makeIMG(links[i].href); console.log(hndl); 
+		document.body.replaceChild(hndl,imghndl); 
+		if ( i < stop ) { funLoop(hndl) }
+	},interval); 
+}
+function begin () { 
+	var first = makeIMG(links[i].href); 
+	document.body.replaceChild(first,heading); 
+	funLoop(first); return first; 
+}
 begin();
